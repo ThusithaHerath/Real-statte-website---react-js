c64ff33cac479481190d872b2ad9f1b99aefb506
Thusitha Herath <83418246+ThusithaHerath@users.noreply.github.com>
2022-06-11 14:33:26 +0530

    reat state website landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Best from './Components/best/Best';
+import Featured from './Components/featured/Featured';
+import Footer from './Components/footer/Footer';
+import Hero from './Components/Hero/Hero';
+import Navbar from './Components/navbar/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <Hero/>
+      <Best/>
+      <Featured/>
+      <Footer/>
+    </>
   );
 }
 
